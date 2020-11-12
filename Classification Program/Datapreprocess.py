d704260e1f5b53ec5983eaa323db6ff33b258f5b
chenxl0220 <xchen435@usc.edu>
2020-11-13 01:01:35 +0800

    test commit
    test commit

--- Classification Program/Datapreprocess.py
+++ Classification Program/Datapreprocess.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from numpy import random
-
 def generate_label0_data(arr_size, p_fever, p_tiredness, p_dry_cough, p_breathing, p_sore_throat, p_pains, p_nasal_congestion,
                          p_runny_nose, p_diarrhea, p_contact_yes, p_contact_no_given_not_yes, p_gender):
     generated_training_inputs, generated_training_labels = ([], [])
@@ -99,8 +98,7 @@ label0_inputs, label0_labels = generate_label0_data(500, 0.3, 0.5, 0.3, 0.3, 0.3
 for i in range(len(label0_inputs)):
     temp = label0_inputs[i]
     temp.append(0)
-
     cleaned_data.loc[len(cleaned_data)] = temp
 
-
+print("changes??")
 cleaned_data.to_csv("processed_input.csv", index=False)
